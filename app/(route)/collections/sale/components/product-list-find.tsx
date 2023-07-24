d7285a06f27411e3275312a2b244374390f554d2
vanthang24803 <nguyenthang24803@gmail.com>
2023-07-24 11:45:32 +0700

    Sale Page Success 👌

--- app/(route)/collections/sale/components/product-list-find.tsx
+++ app/(route)/collections/sale/components/product-list-find.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import MobileFilters from "./mobilefilter";
 import {
   Select,
   SelectContent,
@@ -9,6 +8,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Filter, X } from "lucide-react";
+import { Dialog } from "@headlessui/react";
+import Button from "@/components/ui/button";
 import { Product } from "@/types";
 import ProductCard from "./card";
 
@@ -17,28 +19,40 @@ interface ProductFind {
 }
 
 const ProductFind: React.FC<ProductFind> = ({ items }) => {
-   const [sortedProducts, setSortedProducts] = useState(items);
+  const [sortedProducts, setSortedProducts] = useState(items);
+  const [open, setOpen] = useState(false);
+  const [currentSort, setCurrentSort] = useState("default");
 
-   const handleChange = (value: string) => {
-     if (value === "all") {
-       setSortedProducts([...items]);
-     } else {
-       const tempArray = JSON.parse(JSON.stringify(items)) as Product[];
-       if (value === "high") {
-         tempArray.sort(
-           (product1, product2) =>
-             Number(product1.price) - Number(product2.price)
-         );
-       } else {
-         tempArray.sort(
-           (product1, product2) =>
-             Number(product2.price) - Number(product1.price)
-         );
-       }
-       setSortedProducts(tempArray);
-     }
-   };
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
 
+  // Sort to PC
+  const handleChange = (value: string) => {
+    if (value === "default") {
+      setSortedProducts([...items]);
+    } else {
+      const tempArray = JSON.parse(JSON.stringify(items)) as Product[];
+      if (value === "high") {
+        tempArray.sort(
+          (product1, product2) =>
+            Number(product1.price) - Number(product2.price)
+        );
+      } else {
+        tempArray.sort(
+          (product1, product2) =>
+            Number(product2.price) - Number(product1.price)
+        );
+      }
+      setSortedProducts(tempArray);
+    }
+  };
+
+  // Sort to Mobile
+  const handleSort = (value: string) => {
+    handleChange(value);
+    setCurrentSort(value);
+    onClose();
+  };
 
   return (
     <>
@@ -47,7 +61,6 @@ const ProductFind: React.FC<ProductFind> = ({ items }) => {
           <span className="lg:text-6xl font-medium md:text-5xl text-3xl text-[#00ab84]">
             Best Deals
           </span>
-          <MobileFilters />
           <div className="lg:flex items-center justify-between hidden">
             <span className=" text-neutral-500">
               Save on plants that add texture, color, and style to your sill.
@@ -60,12 +73,96 @@ const ProductFind: React.FC<ProductFind> = ({ items }) => {
                 <SelectValue placeholder="Most Popular" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="all">Most Popular</SelectItem>
+                <SelectItem value="">Most Popular</SelectItem>
                 <SelectItem value="high">$ Low To High</SelectItem>
                 <SelectItem value="low">$ High To Low</SelectItem>
               </SelectContent>
             </Select>
           </div>
+          <>
+            <div className="flex lg:hidden flex-col space-y-4">
+              <span className="text-sm text-neutral-500">
+                Save on plants that add texture, color, and style to your sill.
+                All of our plants ship FREE — no minimums! And they’re
+                guaranteed to arrive happy & healthy.
+              </span>
+              <div className="border-t" onClick={onOpen}>
+                <div className="mt-6 md:py-3 py-2 font-medium border flex items-center justify-center space-x-2">
+                  <span>Most Popular</span>
+                  <Filter size={22} />
+                </div>
+              </div>
+            </div>
+
+            <Dialog
+              open={open}
+              as="div"
+              className="relative z-40 lg:hidden"
+              onClose={onClose}
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+              <div className="fixed inset-0 z-40 flex">
+                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+                  {/* Close button */}
+                  <div className="flex items-center justify-end px-4">
+                    <X size={24} onClick={onClose} />
+                  </div>
+                  <div className="p-4">
+                    <div className="flex flex-col space-y-2 pb-3 border-b">
+                      <span className="font-medium text-xl">Sort Results</span>
+                      <span className="text-neutral-500 text-sm">
+                        {items.length} products
+                      </span>
+                    </div>
+                    <div className="mt-4">
+                      <span className="text-lg font-medium">Sort By:</span>
+                      <div className="grid grid-cols-2 gap-4 mt-2">
+                        <div
+                          className={`w-[120px] h-[45px] flex items-center justify-center font-semibold text-sm border ${
+                            currentSort === "default" &&
+                            "border-neutral-600 italic"
+                          } `}
+                          onClick={() => handleSort("default")}
+                        >
+                          Most Popular
+                        </div>
+                        <div
+                          className={`w-[120px] h-[45px] flex items-center justify-center font-semibold text-sm border ${
+                            currentSort === "high" &&
+                            "border-neutral-600 italic"
+                          }`}
+                          onClick={() => handleSort("high")}
+                        >
+                          $ Low To High
+                        </div>
+                        <div
+                          className={`w-[120px] h-[45px] flex items-center justify-center font-semibold text-sm border ${
+                            currentSort === "low" && "border-neutral-600 italic"
+                          } `}
+                          onClick={() => handleSort("low")}
+                        >
+                          $ High To Low
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="mt-8 flex justify-between ">
+                      <Button onClick={onClose} className="w-1/3">
+                        Close
+                      </Button>
+                      <Button
+                        onClick={onClose}
+                        className="w-1/2 bg-white text-[#009a7b]"
+                        disabled
+                      >
+                        Clear All
+                      </Button>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </div>
+            </Dialog>
+          </>
         </div>
       </div>
       <div className="space-y-4 mt-8">
