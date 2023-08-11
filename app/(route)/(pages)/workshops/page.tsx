4ec93803febb7e5d2eecab946741ca402184e877
vanthang24803 <nguyenthang24803@gmail.com>
2023-08-11 22:16:34 +0700

    ...

--- app/(route)/(pages)/workshops/page.tsx
+++ app/(route)/(pages)/workshops/page.tsx
@@ -3,7 +3,6 @@ import dynamic from "next/dynamic";
 import getBillboard from "@/actions/get-billboard";
 import Billboard from "@/components/ui/billboard";
 import Body from "@/components/ui/body";
-import { Metadata } from "next";
 
 export const data = [
   {
@@ -115,11 +114,6 @@ export const data = [
     href: "/",
   },
 ];
-
-export const metadata: Metadata = {
-  title: `Workshops The Sill`,
-  description: "Buy a flower every day to make life more beautiful. 😊",
-};
 
 
 const WorkShops = async () => {
